a007418a364940a6b2c3f37785d62d6bedb3dce5
fpjjj <www.fpj1010@163.com>
2023-03-22 14:51:34 +0800

    update wakeup_signal.hpp

--- include/behaviortree_cpp/utils/wakeup_signal.hpp
+++ include/behaviortree_cpp/utils/wakeup_signal.hpp
@@ -5,12 +5,15 @@
 #include <mutex>
 #include <condition_variable>
 
+//这是一个简单的线程同步工具，名为WakeUpSignal，用于在多线程环境下等待信号的到来
+
 namespace BT
 {
 
 class WakeUpSignal
 {
 public:
+    //等待信号的到来，最长等待时间为tm，返回值为true表示信号已到达，返回值为false表示等待超时
     /// Return true if the
     bool waitFor(std::chrono::system_clock::duration tm)
     {
@@ -22,6 +25,7 @@ public:
         return res;
     }
 
+    //发送一个信号
     void emitSignal()
     {
        {
@@ -33,9 +37,9 @@ public:
 
 private:
 
-    std::mutex mutex_;
-    std::condition_variable cv_;
-    bool ready_ = false;
+    std::mutex mutex_;  //互斥锁，用于保护条件变量和标志位的访问
+    std::condition_variable cv_;    //条件变量，用于等待信号的到来
+    bool ready_ = false;    //标志位，用于表示信号是否已到达
 };
 
 }
